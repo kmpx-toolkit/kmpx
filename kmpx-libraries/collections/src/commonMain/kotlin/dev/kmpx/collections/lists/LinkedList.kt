3179ca8ed5a26fef1943a861831d634755242673
Jakub Trzebiatowski <jakub.trzebiatowski+git@proton.me>
2025-12-23 18:27:42 +0100

    Implement `LinkedList`

---
+++ kmpx-libraries/collections/src/commonMain/kotlin/dev/kmpx/collections/lists/LinkedList.kt
@@ -0,0 +1,338 @@
+package dev.kmpx.collections.lists
+
+import dev.kmpx.collections.StableCollection.Handle
+import dev.kmpx.collections.internal.data_structures.linked_path.LinkedPath
+import dev.kmpx.collections.internal.data_structures.linked_path.LinkedPath.Node
+import dev.kmpx.collections.internal.data_structures.linked_path.appendAll
+import dev.kmpx.collections.internal.data_structures.linked_path.getNodeAt
+import dev.kmpx.collections.internal.data_structures.linked_path.insertAllBefore
+import dev.kmpx.collections.internal.data_structures.linked_path.traverse
+import kotlin.jvm.JvmInline
+
+/**
+ * A [List] implementation internally based on a double-linked list data structure.
+ *
+ * @param E the type of elements contained in the collection
+ */
+class LinkedList<E>() : AbstractMutableList<E>(), MutableStableList<E> {
+    @JvmInline
+    internal value class LinkedListHandle<E>(
+        val node: Node<E>,
+    ) : Handle<E>
+
+    private var mutableSize: Int = 0
+
+    private val linkedPath = LinkedPath.create<E>()
+
+    override val size: Int
+        get() = mutableSize
+
+    override val handles: Sequence<Handle<E>>
+        get() = linkedPath.traverse().map { it.pack() }
+
+    override fun findEx(
+        element: E,
+    ): Handle<E>? = linkedPath.traverse().find { node ->
+        node.payload == element
+    }?.pack()
+
+    override fun getEx(
+        index: Int,
+    ): Handle<E>? {
+        if (index !in 0..<mutableSize) {
+            return null
+        }
+
+        val node = linkedPath.getNodeAt(index)
+
+        return node.pack()
+    }
+
+    /**
+     * Returns the element at the specified index in the list.
+     *
+     * Guarantees linear time complexity.
+     */
+    override fun get(
+        index: Int,
+    ): E {
+        if (index !in 0..<mutableSize) {
+            throw IndexOutOfBoundsException("Index $index is out of bounds for size ${size}.")
+        }
+
+        val node = linkedPath.getNodeAt(index)
+
+        return node.payload
+
+    }
+
+    /**
+     * Returns the element corresponding to the given handle.
+     *
+     * Guarantees constant time complexity.
+     */
+    override fun getVia(
+        handle: Handle<E>,
+    ): E? {
+        val node = handle.unpack()
+
+        return node.payload
+    }
+
+    /**
+     * Replaces the element at the specified position in this list with the specified element.
+     *
+     * Guarantees linear time complexity.
+     *
+     * @return the element previously at the specified position.
+     */
+    override fun set(
+        index: Int,
+        element: E,
+    ): E {
+        val node = linkedPath.getNodeAt(index)
+
+        val previousElement = node.payload
+
+        node.setPayload(
+            payload = element,
+        )
+
+        return previousElement
+    }
+
+    override fun setVia(
+        handle: Handle<E>,
+        element: E,
+    ): E? {
+        val node = handle.unpack()
+
+        val previousElement = node.payload
+
+        node.setPayload(
+            payload = element,
+        )
+
+        return previousElement
+    }
+
+    /**
+     * Adds the specified element to the end of this list.
+     *
+     * Guarantees constant time complexity.
+     *
+     * @return `true` because the list is always modified as the result of this operation.
+     */
+    override fun add(
+        element: E,
+    ): Boolean {
+        addEx(element = element)
+
+        return true
+    }
+
+    /**
+     * Adds the specified element to the end of this list in exchange for a handle.
+     *
+     * Guarantees constant time complexity.
+     *
+     * @return the handle to the added element.
+     */
+    override fun addEx(
+        element: E,
+    ): Handle<E> {
+        val appendedNode = linkedPath.append(element)
+
+        mutableSize += 1
+
+        return appendedNode.pack()
+    }
+
+    /**
+     * Inserts an element into the list at the specified [index].
+     *
+     * Guarantees linear time complexity.
+     */
+    override fun add(
+        index: Int,
+        element: E,
+    ) {
+        addAtEx(
+            index = index,
+            element = element,
+        )
+    }
+
+    /**
+     * Inserts all the elements of the specified collection [elements] into this list at the specified [index].
+     *
+     * Guarantees linear time complexity.
+     *
+     * @return `true` because the list is always modified as the result of this operation.
+     */
+    override fun addAll(
+        index: Int,
+        elements: Collection<E>,
+    ): Boolean {
+        addAllAt(
+            index = index,
+            elements = elements.toList(),
+        )
+
+        return true
+    }
+
+    /**
+     * Inserts all the elements of the specified collection [elements] into this list at the specified [index].
+     *
+     * Guarantees linear time complexity.
+     */
+    fun addAllAt(
+        index: Int,
+        elements: List<E>,
+    ) {
+        if (index !in 0..size) {
+            throw IndexOutOfBoundsException("Index $index is out of bounds for size ${size}.")
+        }
+
+        when {
+            index == size -> {
+                linkedPath.appendAll(elements)
+            }
+
+            else -> {
+                val referenceNode = linkedPath.getNodeAt(index)
+
+                linkedPath.insertAllBefore(
+                    referenceNode = referenceNode,
+                    payloads = elements,
+                )
+            }
+        }
+
+        mutableSize += elements.size
+    }
+
+    /**
+     * Inserts an element into the list at the specified [index] in exchange for a handle.
+     *
+     * Guarantees linear time complexity.
+     *
+     * @return the handle to the added element.
+     */
+    override fun addAtEx(
+        index: Int,
+        element: E,
+    ): Handle<E> {
+        if (index !in 0..size) {
+            throw IndexOutOfBoundsException("Index $index is out of bounds for size ${size}.")
+        }
+
+        val addedNode = when {
+            index == size -> {
+                linkedPath.append(element)
+            }
+
+            else -> {
+                val referenceNode = linkedPath.getNodeAt(index)
+
+                linkedPath.insertBefore(
+                    referenceNode = referenceNode,
+                    payload = element,
+                )
+            }
+        }
+
+        mutableSize += 1
+
+        return addedNode.pack()
+    }
+
+    /**
+     * Removes an element at the specified [index] from the list.
+     *
+     * Guarantees linear time complexity.
+     *
+     * @return the element that has been removed.
+     */
+    override fun removeAt(
+        index: Int,
+    ): E {
+        if (index !in 0..size) {
+            throw IndexOutOfBoundsException("Index $index is out of bounds for size ${size}.")
+        }
+
+        val node = linkedPath.getNodeAt(index)
+
+        val removedElement = node.payload
+
+        linkedPath.cutOff(node)
+
+        mutableSize -= 1
+
+        return removedElement
+    }
+
+    /**
+     * Removes the element corresponding to the given handle from the list.
+     *
+     * Guarantees linear time complexity.
+     *
+     * @return the element that has been removed.
+     */
+    override fun removeVia(
+        handle: Handle<E>,
+    ): E? {
+        val node = handle.unpack()
+
+        val removedElement = node.payload
+
+        linkedPath.cutOff(node)
+
+        mutableSize -= 1
+
+        return removedElement
+    }
+
+    /**
+     * Returns the index of the element corresponding to the given handle in the list.
+     *
+     * Guarantees linear time complexity.
+     *
+     * @return the index of the element or null if the corresponding element has already been removed
+     */
+    override fun indexOfVia(
+        handle: Handle<E>,
+    ): Int? {
+        val node: Node<E> = handle.unpack()
+
+        val index = linkedPath.traverse().indexOf(node)
+
+        return when {
+            index < 0 -> null
+            else -> index
+        }
+    }
+}
+
+fun <E> linkedListOf(
+    vararg elements: E,
+): LinkedList<E> {
+    val linkedList = LinkedList<E>()
+
+    elements.forEach { element ->
+        linkedList.add(element)
+    }
+
+    return linkedList
+}
+
+private fun <E> Handle<E>.unpack(): Node<E> {
+    this as? LinkedList.LinkedListHandle ?: throw IllegalArgumentException("Handle is not a LinkedListHandle: $this")
+
+    return node
+}
+
+private fun <E> Node<E>.pack(): Handle<E> = LinkedList.LinkedListHandle(
+    node = this,
+)
---
+++ kmpx-libraries/collections/src/commonTest/kotlin/dev/kmpx/collections/lists/LinkedList_tests.kt
@@ -0,0 +1,702 @@
+package dev.kmpx.collections.lists
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFails
+import kotlin.test.assertFalse
+import kotlin.test.assertIs
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
+
+@Suppress("ClassName")
+class LinkedList_tests {
+    private enum class Fruit {
+        Apple, Raspberry, Banana, Orange, Kiwi, Mango, Pineapple, Strawberry, Watermelon, Grape,
+    }
+
+    /**
+     * Test initial state of an empty list.
+     */
+    @Test
+    fun test_initial() {
+        val linkedList = linkedListOf<Fruit>()
+
+        StableListTestUtils.verifyIntegrity(linkedList)
+
+        assertTrue(
+            linkedList.isEmpty(),
+        )
+    }
+
+    /**
+     * Test the [List.get] method implementation.
+     */
+    @Test
+    fun test_get() {
+        val linkedList = linkedListOf(
+            Fruit.Banana,
+            Fruit.Orange,
+            Fruit.Kiwi,
+            Fruit.Strawberry,
+            Fruit.Pineapple,
+        )
+
+        assertEquals(
+            expected = Fruit.Banana,
+            actual = linkedList[0],
+        )
+
+        assertEquals(
+            expected = Fruit.Kiwi,
+            actual = linkedList[2],
+        )
+
+        assertEquals(
+            expected = Fruit.Pineapple,
+            actual = linkedList[4],
+        )
+    }
+
+    /**
+     * Test the [MutableList.set] method implementation.
+     */
+    @Test
+    fun test_set() {
+        val linkedList = linkedListOf(
+            Fruit.Banana,
+            Fruit.Orange,
+            Fruit.Kiwi,
+            Fruit.Strawberry,
+            Fruit.Pineapple,
+        )
+
+        linkedList[1] = Fruit.Raspberry
+
+        StableListTestUtils.verifyIntegrity(linkedList)
+
+        assertEquals(
+            expected = Fruit.Raspberry,
+            actual = linkedList[1],
+        )
+    }
+
+    /**
+     * Test the [MutableList.add] method implementation appending to an empty list.
+     */
+    @Test
+    fun test_add_append_empty() {
+        val linkedList = linkedListOf<Fruit>()
+
+        assertTrue(
+            actual = linkedList.add(Fruit.Grape),
+        )
+
+        StableListTestUtils.verifyIntegrity(linkedList)
+
+        assertEquals(
+            expected = listOf(
+                Fruit.Grape,
+            ),
+            actual = linkedList,
+        )
+    }
+
+    /**
+     * Test the [MutableList.add] method implementation appending to a non-empty list.
+     */
+    @Test
+    fun test_add_append_nonEmpty() {
+        val linkedList = linkedListOf(
+            Fruit.Grape,
+            Fruit.Strawberry,
+        )
+
+        assertTrue(
+            actual = linkedList.add(Fruit.Orange),
+        )
+
+        StableListTestUtils.verifyIntegrity(linkedList)
+
+        assertEquals(
+            expected = listOf(
+                Fruit.Grape,
+                Fruit.Strawberry,
+                Fruit.Orange,
+            ),
+            actual = linkedList,
+        )
+    }
+
+    /**
+     * Test the [MutableList.add] method implementation appending a duplicate element.
+     */
+    @Test
+    fun test_add_append_duplicate() {
+        val linkedList = linkedListOf(
+            Fruit.Grape,
+            Fruit.Strawberry,
+            Fruit.Apple,
+            Fruit.Mango,
+        )
+
+        assertTrue(
+            actual = linkedList.add(Fruit.Apple),
+        )
+
+        StableListTestUtils.verifyIntegrity(linkedList)
+
+        assertEquals(
+            expected = listOf(
+                Fruit.Grape,
+                Fruit.Strawberry,
+                Fruit.Apple,
+                Fruit.Mango,
+                Fruit.Apple,
+            ),
+            actual = linkedList,
+        )
+    }
+
+    /**
+     * Test the [MutableStableList.addEx] method implementation.
+     */
+    @Test
+    fun test_addEx() {
+        val linkedList = linkedListOf(
+            Fruit.Banana,
+            Fruit.Orange,
+            Fruit.Pineapple,
+        )
+
+        val addedElementHandle = linkedList.addEx(Fruit.Banana)
+
+        StableListTestUtils.verifyIntegrity(linkedList)
+
+        assertEquals(
+            expected = listOf(
+                Fruit.Banana,
+                Fruit.Orange,
+                Fruit.Pineapple,
+                Fruit.Banana,
+            ),
+            actual = linkedList,
+        )
+
+        assertEquals(
+            expected = Fruit.Banana,
+            actual = linkedList.getVia(handle = addedElementHandle),
+        )
+    }
+
+    /**
+     * Test the [MutableList.add] method implementation inserting at index 0.
+     */
+    @Test
+    fun test_add_atIndex_first() {
+        val linkedList = linkedListOf(
+            Fruit.Grape,
+            Fruit.Strawberry,
+            Fruit.Orange,
+        )
+
+        linkedList.add(
+            index = 0,
+            element = Fruit.Apple,
+        )
+
+        StableListTestUtils.verifyIntegrity(linkedList)
+
+        assertEquals(
+            expected = listOf(
+                Fruit.Apple,
+                Fruit.Grape,
+                Fruit.Strawberry,
+                Fruit.Orange,
+            ),
+            actual = linkedList,
+        )
+    }
+
+    /**
+     * Test the [MutableList.add] method implementation inserting at a middle index.
+     */
+    @Test
+    fun test_add_atIndex_middle() {
+        val linkedList = linkedListOf(
+            Fruit.Strawberry,
+            Fruit.Grape,
+            Fruit.Watermelon,
+            Fruit.Orange,
+        )
+
+        linkedList.add(
+            index = 2,
+            element = Fruit.Apple,
+        )
+
+        StableListTestUtils.verifyIntegrity(linkedList)
+
+        assertEquals(
+            expected = listOf(
+                Fruit.Strawberry,
+                Fruit.Grape,
+                Fruit.Apple,
+                Fruit.Watermelon,
+                Fruit.Orange,
+            ),
+            actual = linkedList,
+        )
+    }
+
+    /**
+     * Test the [MutableList.add] method implementation inserting an element equal to an existing one.
+     */
+    @Test
+    fun test_add_atIndex_duplicate() {
+        val linkedList = linkedListOf(
+            Fruit.Strawberry,
+            Fruit.Grape,
+            Fruit.Watermelon,
+            Fruit.Orange,
+        )
+
+        linkedList.add(
+            index = 2,
+            element = Fruit.Watermelon,
+        )
+
+        StableListTestUtils.verifyIntegrity(linkedList)
+
+        assertEquals(
+            expected = listOf(
+                Fruit.Strawberry,
+                Fruit.Grape,
+                Fruit.Watermelon,
+                Fruit.Watermelon,
+                Fruit.Orange,
+            ),
+            actual = linkedList,
+        )
+    }
+
+    /**
+     * Test the [MutableList.add] method implementation inserting at the last index.
+     */
+    @Test
+    fun test_add_atIndex_last() {
+        val linkedList = linkedListOf(
+            Fruit.Strawberry,
+            Fruit.Grape,
+            Fruit.Orange,
+        )
+
+        linkedList.add(
+            index = 2,
+            element = Fruit.Apple,
+        )
+
+        StableListTestUtils.verifyIntegrity(linkedList)
+
+        assertEquals(
+            expected = listOf(
+                Fruit.Strawberry,
+                Fruit.Grape,
+                Fruit.Apple,
+                Fruit.Orange,
+            ),
+            actual = linkedList,
+        )
+    }
+
+    /**
+     * Test the [MutableList.add] method implementation inserting one past the last index.
+     */
+    @Test
+    fun test_add_atIndex_onePastLast() {
+        val linkedList = linkedListOf(
+            Fruit.Strawberry,
+            Fruit.Grape,
+            Fruit.Orange,
+        )
+
+        linkedList.add(
+            index = 3,
+            element = Fruit.Apple,
+        )
+
+        StableListTestUtils.verifyIntegrity(linkedList)
+
+        assertEquals(
+            expected = listOf(
+                Fruit.Strawberry,
+                Fruit.Grape,
+                Fruit.Orange,
+                Fruit.Apple,
+            ),
+            actual = linkedList,
+        )
+    }
+
+    /**
+     * Test the [MutableStableList.addAtEx] method implementation (adding a single element).
+     */
+    @Test
+    fun test_addAtEx_single() {
+        val linkedList = linkedListOf(
+            Fruit.Grape,
+            Fruit.Strawberry,
+            Fruit.Orange,
+        )
+
+        val addedElementHandle = linkedList.addAtEx(
+            index = 1,
+            element = Fruit.Apple,
+        )
+
+        StableListTestUtils.verifyIntegrity(linkedList)
+
+        assertEquals(
+            expected = listOf(
+                Fruit.Grape,
+                Fruit.Apple,
+                Fruit.Strawberry,
+                Fruit.Orange,
+            ),
+            actual = linkedList,
+        )
+
+        assertEquals(
+            expected = Fruit.Apple,
+            actual = linkedList.getVia(handle = addedElementHandle),
+        )
+    }
+
+    /**
+     * Test the [MutableStableList.addAtEx] method implementation (adding multiple elements).
+     */
+    @Test
+    fun test_addAtEx_multiple() {
+        val linkedList = linkedListOf(
+            Fruit.Strawberry,
+            // +Mango
+            Fruit.Kiwi,
+            Fruit.Orange,
+            // +Pineapple
+            Fruit.Grape,
+            Fruit.Raspberry,
+            // +Kiwi
+            Fruit.Orange,
+        )
+
+        linkedList.addAtEx(
+            index = 5,
+            element = Fruit.Kiwi,
+        )
+
+        StableListTestUtils.verifyIntegrity(linkedList)
+
+        linkedList.addAtEx(
+            index = 3,
+            element = Fruit.Pineapple,
+        )
+
+        StableListTestUtils.verifyIntegrity(linkedList)
+
+        linkedList.addAtEx(
+            index = 1,
+            element = Fruit.Mango,
+        )
+        StableListTestUtils.verifyIntegrity(linkedList)
+
+        assertEquals(
+            expected = listOf(
+                Fruit.Strawberry,
+                Fruit.Mango,
+                Fruit.Kiwi,
+                Fruit.Orange,
+                Fruit.Pineapple,
+                Fruit.Grape,
+                Fruit.Raspberry,
+                Fruit.Kiwi,
+                Fruit.Orange,
+            ),
+            actual = linkedList,
+        )
+    }
+
+    /**
+     * Test the [MutableList.addAll] method implementation inserting at the last index.
+     */
+    @Test
+    fun test_addAllAt() {
+        val linkedList = linkedListOf(
+            Fruit.Strawberry,
+            Fruit.Grape,
+            Fruit.Orange,
+        )
+
+        linkedList.addAllAt(
+            index = 2,
+            elements = listOf(
+                Fruit.Pineapple,
+                Fruit.Apple,
+                Fruit.Raspberry,
+            ),
+        )
+
+        StableListTestUtils.verifyIntegrity(linkedList)
+
+        assertEquals(
+            expected = listOf(
+                Fruit.Strawberry,
+                Fruit.Grape,
+                Fruit.Pineapple,
+                Fruit.Apple,
+                Fruit.Raspberry,
+                Fruit.Orange,
+            ),
+            actual = linkedList,
+        )
+    }
+
+    /**
+     * Test the [MutableList.removeAt] method implementation removing from the first index.
+     */
+    @Test
+    fun test_removeAt_first() {
+        val linkedList = linkedListOf(
+            Fruit.Strawberry,
+            Fruit.Grape,
+            Fruit.Orange,
+        )
+
+        linkedList.removeAt(0)
+
+        StableListTestUtils.verifyIntegrity(linkedList)
+
+        assertEquals(
+            expected = listOf(
+                Fruit.Grape,
+                Fruit.Orange,
+            ),
+            actual = linkedList,
+        )
+    }
+
+    /**
+     * Test the [MutableList.removeAt] method implementation removing from a middle index.
+     */
+    @Test
+    fun test_removeAt_middle() {
+        val linkedList = linkedListOf(
+            Fruit.Strawberry,
+            Fruit.Kiwi,
+            Fruit.Orange,
+        )
+
+        linkedList.removeAt(1)
+
+        StableListTestUtils.verifyIntegrity(linkedList)
+
+        assertEquals(
+            expected = listOf(
+                Fruit.Strawberry,
+                Fruit.Orange,
+            ),
+            actual = linkedList,
+        )
+    }
+
+    /**
+     * Test the [MutableList.removeAt] method implementation removing from the last index.
+     */
+    @Test
+    fun test_removeAt_last() {
+        val linkedList = linkedListOf(
+            Fruit.Strawberry,
+            Fruit.Grape,
+            Fruit.Orange,
+        )
+
+        linkedList.removeAt(2)
+
+        StableListTestUtils.verifyIntegrity(linkedList)
+
+        assertEquals(
+            expected = listOf(
+                Fruit.Strawberry,
+                Fruit.Grape,
+            ),
+            actual = linkedList,
+        )
+    }
+
+    /**
+     * Test the [MutableList.removeAt] method implementation removing from an index past the last index.
+     */
+    @Test
+    fun test_removeAt_pastLast() {
+        val linkedList = linkedListOf(
+            Fruit.Strawberry,
+            Fruit.Grape,
+            Fruit.Orange,
+        )
+
+        assertIs<IndexOutOfBoundsException>(
+            assertFails {
+                linkedList.removeAt(3)
+            },
+        )
+
+        StableListTestUtils.verifyIntegrity(linkedList)
+
+        assertEquals(
+            expected = listOf(
+                Fruit.Strawberry,
+                Fruit.Grape,
+                Fruit.Orange,
+            ),
+            actual = linkedList,
+        )
+    }
+
+    /**
+     * Test the [MutableList.remove] method implementation removing a contained element.
+     */
+    @Test
+    fun test_remove_contained() {
+        val linkedList = linkedListOf(
+            Fruit.Strawberry,
+            Fruit.Grape,
+            Fruit.Orange,
+        )
+
+        assertTrue(
+            actual = linkedList.remove(
+                Fruit.Strawberry,
+            ),
+        )
+
+        StableListTestUtils.verifyIntegrity(linkedList)
+
+        assertEquals(
+            expected = listOf(
+                Fruit.Grape,
+                Fruit.Orange,
+            ),
+            actual = linkedList,
+        )
+    }
+
+    /**
+     * Test the [MutableList.remove] method implementation removing a non-contained element.
+     */
+    @Test
+    fun test_remove_nonContained() {
+        val linkedList = linkedListOf(
+            Fruit.Strawberry,
+            Fruit.Kiwi,
+            Fruit.Orange,
+        )
+
+        assertFalse(
+            actual = linkedList.remove(Fruit.Apple),
+        )
+
+        StableListTestUtils.verifyIntegrity(linkedList)
+
+        assertEquals(
+            expected = listOf(
+                Fruit.Strawberry,
+                Fruit.Kiwi,
+                Fruit.Orange,
+            ),
+            actual = linkedList,
+        )
+    }
+
+    /**
+     * Test the [MutableStableList.removeVia] method implementation.
+     */
+    @Test
+    fun test_removeVia() {
+        val linkedList = linkedListOf(
+            Fruit.Strawberry,
+            Fruit.Kiwi,
+            Fruit.Orange,
+        )
+
+        val addedElementHandle = linkedList.addAtEx(
+            index = 2,
+            element = Fruit.Mango,
+        )
+
+        assertEquals(
+            expected = Fruit.Mango,
+            actual = linkedList.removeVia(
+                handle = addedElementHandle,
+            ),
+        )
+
+        StableListTestUtils.verifyIntegrity(linkedList)
+
+        assertEquals(
+            expected = listOf(
+                Fruit.Strawberry,
+                Fruit.Kiwi,
+                Fruit.Orange,
+            ),
+            actual = linkedList,
+        )
+    }
+
+    /**
+     * Test the [MutableStableList.indexOfVia] method implementation.
+     */
+    @Test
+    fun test_indexOfVia() {
+        val linkedList = linkedListOf(
+            Fruit.Strawberry,
+            Fruit.Kiwi,
+            Fruit.Orange,
+            Fruit.Grape,
+        )
+
+        val elementHandle = linkedList.findEx(Fruit.Orange)!!
+
+        assertEquals(
+            expected = 2,
+            actual = linkedList.indexOfVia(
+                handle = elementHandle,
+            ),
+        )
+    }
+
+    /**
+     * Test the [StableList.findEx] method implementation.
+     */
+    @Test
+    fun test_findEx() {
+        val linkedList = linkedListOf(
+            Fruit.Strawberry,
+            Fruit.Kiwi,
+            Fruit.Orange,
+            Fruit.Kiwi,
+        )
+
+        val handle = assertNotNull(
+            linkedList.findEx(Fruit.Kiwi),
+        )
+
+        assertEquals(
+            expected = Fruit.Kiwi,
+            actual = linkedList.getVia(handle = handle),
+        )
+
+        assertEquals(
+            expected = 1,
+            actual = linkedList.indexOfVia(handle = handle),
+        )
+    }
+}
